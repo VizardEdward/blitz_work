a069bd27a6a7d977fd8cac4437c3f9beb813eabc
VizardEdward <lordedward98@gmail.com>
2021-09-16 21:25:48 -0400

    Fixed typo

--- blitz_work/internationalization.py
+++ blitz_work/internationalization.py
@@ -7,6 +7,6 @@ DELETE_TITLE = ["Delete", "Eliminar"]
 UPDATE_TITLE = ["Edit", "Modificar"]
 DETAIL_TITLE = ["Detail", "Detalles"]
 DELETE_TEXT = ["The following elements will be deleted, do you want to delete them?",
-               "Los siguientes elementofs serán eliminados, ¿Está seguro?"]
+               "Los siguientes elementos serán eliminados, ¿Está seguro?"]
 CRUD_BUTTONS = [{"add": "Add", "create": "Create", "details": "Details", "update": "Update", "edit": "Edit", "delete": "Delete", "cancel": "Cancel", "return": "Return", "search": "Search"}, {
     "add": "Añadir", "create": "Crear", "details": "Detalles", "update": "Actualizar", "edit": "Editar", "delete": "Eliminar", "cancel": "Cancelar", "return": "Regresar", "search": "Buscar"}]
